e92c8dafa3f4526238f06655aedaec133da6adee
sung eun <78583325+sungeunn@users.noreply.github.com>
2024-04-01 21:11:24 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int mul = 1;
+        int sum = 0;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            mul *= num_list[i];
+            sum += num_list[i];
+        }
+        
+        if (mul < (sum*sum)) {
+            return 1;
+        } else {
+            return 0;
+        }
+        
+    }
+}
