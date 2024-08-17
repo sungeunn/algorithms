86297b1883d5135715e1a2eb2f24c1f3c08aca24
sung eun <78583325+sungeunn@users.noreply.github.com>
2024-08-17 23:33:48 +0900

    [level 0] Title: 7의 개수, Time: 13.88 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        String str = "";
+        for (int i : array) {
+            str += i;
+        }
+        
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '7') {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
