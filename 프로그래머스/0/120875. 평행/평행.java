26b194fad8b4f42391c19ac741f962e32c62ed7e
sung eun <78583325+sungeunn@users.noreply.github.com>
2025-03-08 23:36:38 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[][] dots) {
+        if (isParallel(dots[0], dots[1], dots[2], dots[3])) return 1;
+        if (isParallel(dots[0], dots[2], dots[1], dots[3])) return 1;
+        if (isParallel(dots[0], dots[3], dots[1], dots[2])) return 1;
+        
+        return 0;
+    }
+    
+    private boolean isParallel(int[] p1, int[] p2, int[] p3, int[] p4) {
+        return (p2[1] - p1[1]) * (p4[0] - p3[0]) == (p4[1] - p3[1]) * (p2[0] - p1[0]);
+    }
+}
