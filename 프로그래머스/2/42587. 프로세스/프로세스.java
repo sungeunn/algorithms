a655ad1723ce3672b226b68ed27c765e86356d94
sung eun <78583325+sungeunn@users.noreply.github.com>
2025-05-09 23:15:21 +0900

    [level 2] Title: 프로세스, Time: 1.85 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(new int[] { i, priorities[i] });
+        }
+
+        int answer = 0;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            boolean hasHigher = false;
+
+            for (int[] q : queue) {
+                if (q[1] > current[1]) {
+                    hasHigher = true;
+                    break;
+                }
+            }
+
+            if (hasHigher) {
+                queue.add(current);
+            } else {
+                answer++;
+                if (current[0] == location) {
+                    return answer;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
